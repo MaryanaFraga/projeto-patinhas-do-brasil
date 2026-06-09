eb6acbe5737175f0ce96c7a49cdadf4434d58df3
Maryana Fraga <fraga.azul.maryana@gmail.com>
2026-06-09 20:04:07 -0300

    feat: cria model Animal e funções de mudança de status

---
+++ models/Animal.py
@@ -0,0 +1,32 @@
+class Animal:
+    
+    STATUS_AVAILABLE = 'disponivel'
+    STATUS_ADOPTED = 'adotado'
+    STATUS_PROCESSING = 'em processo'
+    
+    def __init__(self, name, species, breed, age, sex, size, status=STATUS_AVAILABLE, description='', image=None, id=None):
+        self.id = id
+        self.name = name
+        self.species = species
+        self.breed = breed
+        self.age = age
+        self.sex = sex
+        self.size = size
+        self.status = status
+        self.description = description
+        self.image = image
+
+    def __str__(self):
+        return f'{self.name} is a {self.age}-year-old {self.sex} {self.breed} ({self.species}) currently located at {self.size}. Status: {self.status}. Description: {self.description}. Imagem: {self.image}'
+
+    def is_available(self):
+        return self.status == self.STATUS_AVAILABLE
+    
+    def mark_as_processing(self):
+        if not self.is_available():
+            raise Exception('Animal não disponível para adoção')
+        else:
+            self.status = self.STATUS_PROCESSING
+
+    def mark_as_adopted(self):
+        self.status = self.STATUS_ADOPTED
