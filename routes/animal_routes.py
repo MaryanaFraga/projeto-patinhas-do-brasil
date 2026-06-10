7078c363cc019bb7d7464ec537dad9904ec4ed79
Maryana Fraga <fraga.azul.maryana@gmail.com>
2026-06-09 22:33:48 -0300

    fix: Adicionar configurações corretas para uso de métodos http

--- routes/animal_routes.py
+++ routes/animal_routes.py
@@ -1,27 +1,31 @@
-from flask import Blueprint, request, jsonify
-from models import Animal
+from flask import Blueprint, redirect, render_template, request, url_for
+from models.Animal import Animal
 from services.animal_service import AnimalService
 
 animal_routes = Blueprint('animal_routes', __name__)
 
-@animal_routes.route('/animals', methods=['POST'])
+@animal_routes.route('/animals/add_animal', methods=['GET','POST'])
 def add_animal():
-    data = request.get_json()
-    
-    new = AnimalService.add_animal(Animal(
-        name=data['name'],
-        species=data['species'],
-        breed=data['breed'],
-        age=data['age'],
-        sex=data['sex'],
-        size=data['size'],
-        status=data['status'],
-        description=data['description'],
-        image=data['image']
-    ))
-    return jsonify({"Message": "Animal added successfully", "Animal": new.__dict__}), 201
+    if request.method == 'POST':
+        new = Animal(
+            name= request.form.get('name'),
+            species=request.form.get('species'),
+            breed=request.form.get('breed'),
+            age=request.form.get('age'),
+            sex=request.form.get('sex'),
+            size=request.form.get('size'),
+            status=request.form.get('status'),
+            description=request.form.get('description'),
+            image=request.form.get('image')
+        )
+        
+        AnimalService.add_animal(new)
+        return redirect(url_for('animal_routes.add_animal'))
 
-@animal_routes.route('/animals', methods=['GET'])
-def view_animals():
-    animals = AnimalService.view_animals()
-    return jsonify({"Message": "Animals retrieved successfully", "Animals": [animal.__dict__ for animal in animals]}), 200
+    animals = AnimalService.list_animals()
+    return render_template('animals/add_animal.html', animals=animals)
+
+@animal_routes.route('/animals/list_animals', methods=['GET'])
+def list_animals():
+    animals = AnimalService.list_animals()
+    return render_template('animals/list_animals.html', animals=animals)
