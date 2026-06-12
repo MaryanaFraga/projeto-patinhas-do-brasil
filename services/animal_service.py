850fae2c28762a3917ef1f44dbd49fbc8d5b58df
Maryana Fraga <fraga.azul.maryana@gmail.com>
2026-06-11 23:57:32 -0300

    refactor: Mudar nomes e posições de funções (+ corrigir path de imagem)

--- services/animal_service.py
+++ services/animal_service.py
@@ -9,7 +9,7 @@ class AnimalService:
         cursor.execute('''
             INSERT INTO animais (name, species, breed, age, sex, size, status, description, image)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-        ''', (animal.name, animal.species, animal.breed, animal.age, animal.sex, animal.size, animal.status, animal.description, animal.image))
+        ''', (animal.name, animal.species, animal.breed, animal.age, animal.sex, animal.size, animal.status, animal.description, '/static/'+animal.image))
 
         conn.commit()
         conn.close()
@@ -28,15 +28,6 @@ class AnimalService:
         conn.close()
     
     @staticmethod
-    def remove_animal(animal_id):
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute('DELETE FROM animais WHERE id = ?', (animal_id,))
-
-        conn.commit()
-        conn.close()
-    
-    @staticmethod
     def list_animals():
         conn = get_db_connection()
         cursor = conn.cursor()
@@ -45,3 +36,13 @@ class AnimalService:
         
         conn.close()
         return rows
+    
+    @staticmethod
+    def remove_registry(id):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('DELETE FROM animais WHERE id = ?', (id,))
+
+        conn.commit()
+        conn.close()
+    
